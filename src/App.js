5b8e84037f10bc9e0fecf675ad1d0c8b80c86eed
Pablo Castillo <psc88sdrd@gmail.com>
2021-12-08 00:25:49 -0300

    se agrego enrutamiento

--- src/App.js
+++ src/App.js
@@ -1,11 +1,16 @@
 import './App.css';
 import Inicio from './Pages/Inicio';
+import AcercaNosotros from './Pages/AcercaNosotros';
+import {BrowserRouter as Router, Routes, Route} from 'react-router-dom'
 
 function App() {
   return (
-    <div>
-      <Inicio></Inicio>
-    </div>
+    <Router>
+      <Routes>
+        <Route exact path='/' element={<Inicio></Inicio>}></Route>
+        <Route exact path='/nosotros' element={<AcercaNosotros></AcercaNosotros>}></Route>
+      </Routes>
+    </Router>
   );
 }
 
---
+++ src/Pages/AcercaNosotros.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const AcercaNosotros = () => {
+    return (
+        <div>
+            <h1>Esta es la pagina acerca Nosotros</h1>
+        </div>
+    );
+};
+
+export default AcercaNosotros;
