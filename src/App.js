f2c357d4ab6c214aed0c9ad531f60b25ac7552d7
AngelMarcelo <ammarce@gmail.com>
2021-12-08 19:16:41 -0300

    se agrega carpeta components, se agrega imagen de logo. Tambien se agregan dependencias bootstrap jquery popper.js

--- src/App.js
+++ src/App.js
@@ -1,12 +1,20 @@
 import './App.css';
+import Navigation from './components/common/Navigation';
 import Inicio from './Pages/Inicio';
 import AcercaNosotros from './Pages/AcercaNosotros';
 import {BrowserRouter as Router, Routes, Route} from 'react-router-dom'
 import 'bootstrap/dist/css/bootstrap.min.css';
+import { Navbar, Nav, Container } from "react-bootstrap";
+import 'bootstrap';
+import 'bootstrap/dist/css/bootstrap.css';
+import 'bootstrap/dist/js/bootstrap.js';
+import $ from 'jquery';
+import Popper from 'popper.js';
 
 function App() {
   return (
     <Router>
+      <Navigation></Navigation>
       <Routes>
         <Route exact path='/' element={<Inicio></Inicio>}></Route>
         <Route exact path='/nosotros' element={<AcercaNosotros></AcercaNosotros>}></Route>
--- src/Pages/Inicio.js
+++ src/Pages/Inicio.js
@@ -3,7 +3,7 @@ import React from 'react';
 const Inicio = () => {
     return (
         <div>
-            <h1>Hola mundo</h1>
+            
         </div>
     );
 };
