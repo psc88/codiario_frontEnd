2b673f029b907eb9645cc8c4bc1305090964cda3
AngelMarcelo <ammarce@gmail.com>
2021-12-08 20:29:39 -0300

    se corrige codigo en navigation

--- src/components/common/Navigation.js
+++ src/components/common/Navigation.js
@@ -12,24 +12,33 @@ const Navigation = () =>{
         <>
           <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
              <Container>
-             <Navbar.Brand href="#home">React-Bootstrap</Navbar.Brand>
+             <Navbar.Brand href="#home">
+             <img
+                   src="../../img/Logo.png"
+                   width="20"
+                   height="14"
+                   className="d-inline-block align-top"
+                   alt="<CoDiario> logo"
+                 />
+             </Navbar.Brand>
              <Navbar.Toggle aria-controls="responsive-navbar-nav" />
              <Navbar.Collapse id="responsive-navbar-nav">
                <Nav className="me-auto">
-                 <Nav.Link href="#features">Features</Nav.Link>
-                 <Nav.Link href="#pricing">Pricing</Nav.Link>
+                 <Nav.Link href="#actualidad">Actualidad</Nav.Link>
+                 <Nav.Link href="#espectaculos">Espectáculos</Nav.Link>
+                 <Nav.Link href="#tecnologia">Tecnología</Nav.Link>
+                 <Nav.Link href="#deportes">Deportes</Nav.Link>
                  <NavDropdown title="Dropdown" id="collasible-nav-dropdown">
-                   <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
-                   <NavDropdown.Item href="#action/3.2">Another action</NavDropdown.Item>
-                   <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
-                   <NavDropdown.Divider />
-                   <NavDropdown.Item href="#action/3.4">Separated link</NavDropdown.Item>
-                 </NavDropdown>
+                   <NavDropdown.Item href="#action/3.1">Política</NavDropdown.Item>
+                   <NavDropdown.Item href="#action/3.2">Economía</NavDropdown.Item>
+                   <NavDropdown.Item href="#action/3.3">Salud</NavDropdown.Item>
+                   <NavDropdown.Item href="#action/3.4">Fotografías</NavDropdown.Item>
+                  </NavDropdown>
                </Nav>
                <Nav>
-                 <Nav.Link href="#deets">More deets</Nav.Link>
-                 <Nav.Link eventKey={2} href="#memes">
-                   Dank memes
+                 <Nav.Link href="#suscribirse" variant="primary" type="button">Suscribirse</Nav.Link>
+                 <Nav.Link eventKey={2} href="#sesion" variant="warning" type="button">
+                   Iniciar Sesión
                  </Nav.Link>
                </Nav>
              </Navbar.Collapse>
