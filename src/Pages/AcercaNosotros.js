edff987a2d54e41b17a55999c3527a538a6965fa
Pablo Castillo <psc88sdrd@gmail.com>
2021-12-08 21:20:52 -0300

    se agrego formate de letras y cambio en boton

--- src/Pages/AcercaNosotros.js
+++ src/Pages/AcercaNosotros.js
@@ -5,15 +5,15 @@ import pabloc from '../img/pabloCastillo.jfif';
 import pablod from '../img/pabloDotti.jfif';
 import gabriel from '../img/gabrielAlejandro.jfif';
 import marcelo from '../img/marceloMartinez.jfif';
-import { Link } from 'react-router-dom';
+import '../App.css';
 
 const AcercaNosotros = () => {
     return (
         <>
             <Container className='text-center'>
                 <article className='text-center my-5'>
-                    <h1>Nuestra Historia</h1>
-                    <p>Somos estudiantes de Desarrollo web Full Stack, apasionados por el desarrollo y la tecnologia. Este espacio es para que nos conozcan un poco mas </p>
+                    <h1 id='tituloNosotros'>Nuestra Historia</h1>
+                    <p id='subtituloNosotros'>Somos estudiantes de Desarrollo web Full Stack, apasionados por el desarrollo y la tecnologia. Este espacio es para que nos conozcan un poco mas </p>
                     <hr></hr>
                 </article>
                 <Row>
@@ -21,11 +21,11 @@ const AcercaNosotros = () => {
                         <Card className='mt-2 border-0 shadow-lg p-3 mb-5 bg-body rounded'>
                             <Card.Img variant="top rounded-circle p-2" src={pabloc} />
                             <Card.Body>
-                                <Card.Title>Pablo Sebastián Castillo</Card.Title>
-                                <Card.Text>
+                                <Card.Title className='fs-2'>Pablo Sebastián Castillo</Card.Title>
+                                <Card.Text className='fs-5'>
                                     Estudiante de Ing. en Sistemas (3er año) y desarrollador web FullStack. Me apasiona programar y enfrantar nuevos retos
-                                    <div>
-                                        <a href='https://portafolio-pablo-castillo.netlify.app' className='btn btn-primary mt-2' _blank>Conoceme</a>
+                                    <div className='pt-4'>
+                                        <a href='https://portafolio-pablo-castillo.netlify.app' className='btn btn-primary mt-2 fs-4' rel='noreferrer' target="_blank">Conoceme</a>
                                     </div>
                                 </Card.Text>
                             </Card.Body>
@@ -35,11 +35,11 @@ const AcercaNosotros = () => {
                         <Card className='mt-2 border-0 shadow-lg p-3 mb-5 bg-body rounded'>
                             <Card.Img variant="top rounded-circle p-2" src={enrico} />
                             <Card.Body>
-                                <Card.Title>Enrico Palermo</Card.Title>
-                                <Card.Text>
+                                <Card.Title className='fs-2'>Enrico Palermo</Card.Title>
+                                <Card.Text className='fs-5'>
                                     Estudiante de Ing. en Sistemas (3er año) y desarrollador web FullStack. Me apasiona programar y enfrantar nuevos retos
-                                    <div>
-                                        <a href='https://portafolio-pablo-castillo.netlify.app' className='btn btn-primary mt-2' _blank>Conoceme</a>
+                                    <div className='pt-4'>
+                                        <a href='https://portafolio-pablo-castillo.netlify.app' className='btn btn-primary mt-2 fs-4' rel='noreferrer' target="_blank">Conoceme</a>
                                     </div>
                                 </Card.Text>
                             </Card.Body>
@@ -49,11 +49,11 @@ const AcercaNosotros = () => {
                         <Card className='mt-2 border-0 shadow-lg p-3 mb-5 bg-body rounded'>
                             <Card.Img variant="top rounded-circle p-2" src={pablod} />
                             <Card.Body>
-                                <Card.Title>Pablo Gabriel Dotti</Card.Title>
-                                <Card.Text>
+                                <Card.Title className='fs-2'>Pablo Gabriel Dotti</Card.Title>
+                                <Card.Text className='fs-5'>
                                     Estudiante de Ing. en Sistemas (3er año) y desarrollador web FullStack. Me apasiona programar y enfrantar nuevos retos
-                                    <div>
-                                        <a href='https://portafolio-pablo-castillo.netlify.app' className='btn btn-primary mt-2' _blank>Conoceme</a>
+                                    <div className='pt-4'>
+                                        <a href='https://portafolio-pablo-castillo.netlify.app' className='btn btn-primary mt-2 fs-4' rel='noreferrer' target="_blank">Conoceme</a>
                                     </div>
                                 </Card.Text>
                             </Card.Body>
@@ -65,11 +65,11 @@ const AcercaNosotros = () => {
                         <Card className='mt-2 border-0 shadow-lg p-3 mb-5 bg-body rounded'>
                             <Card.Img variant="top rounded-circle p-2" src={marcelo} />
                             <Card.Body>
-                                <Card.Title>Marcelo Martinez</Card.Title>
-                                <Card.Text>
+                                <Card.Title className='fs-2'>Marcelo Martinez</Card.Title>
+                                <Card.Text className='fs-5'>
                                     Estudiante de Ing. en Sistemas (3er año) y desarrollador web FullStack. Me apasiona programar y enfrantar nuevos retos
-                                    <div>
-                                        <a href='https://portafolio-pablo-castillo.netlify.app' className='btn btn-primary mt-2' _blank>Conoceme</a>
+                                    <div className='pt-4'>
+                                        <a href='https://www.linkedin.com/in/marcelo-martinez-47791542' className='btn btn-primary mt-2 fs-4' rel='noreferrer' target="_blank">Conoceme</a>
                                     </div>
                                 </Card.Text>
                             </Card.Body>
@@ -79,11 +79,11 @@ const AcercaNosotros = () => {
                         <Card className='mt-2 border-0 shadow-lg p-3 mb-5 bg-body rounded'>
                             <Card.Img variant="top rounded-circle p-2" src={gabriel} />
                             <Card.Body>
-                                <Card.Title>Gabriel Alejandro</Card.Title>
-                                <Card.Text>
+                                <Card.Title className='fs-2'>Gabriel Alejandro</Card.Title>
+                                <Card.Text className='fs-5'>
                                     Estudiante de Ing. en Sistemas (3er año) y desarrollador web FullStack. Me apasiona programar y enfrantar nuevos retos
-                                    <div>
-                                        <a href='https://portafolio-pablo-castillo.netlify.app' className='btn btn-primary mt-2' _blank>Conoceme</a>
+                                    <div className='pt-4'>
+                                        <a href='https://portafolio-pablo-castillo.netlify.app' className='btn btn-primary mt-2 fs-4' rel='noreferrer' target="_blank">Conoceme</a>
                                     </div>
                                 </Card.Text>
                             </Card.Body>
