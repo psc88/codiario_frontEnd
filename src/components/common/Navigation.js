c9830af4f48ed7c9ca4bf8f054938fe9b5999c79
AngelMarcelo <ammarce@gmail.com>
2021-12-11 14:43:37 -0300

    ajuste de codigo en navigation desde login

--- src/components/common/Navigation.js
+++ src/components/common/Navigation.js
@@ -13,44 +13,45 @@ import { Button } from "react-bootstrap";
 const Navigation = () =>{
     return(
         <>
-          <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
-             <Container>
-             <Navbar.Brand href="#home">
-             <img
-                   src={logo}
-                   width="120"
-                   height="56"
-                   className="d-inline-block align-top"
-                   alt="<CoDiario>"
-                 />
-             </Navbar.Brand>
-             <Navbar.Toggle aria-controls="responsive-navbar-nav" />
-             <Navbar.Collapse id="responsive-navbar-nav">
-               <Nav className="me-auto">
-                 <Nav.Link href="#actualidad">Actualidad</Nav.Link>
-                 <Nav.Link href="#espectaculos">Espectáculos</Nav.Link>
-                 <Nav.Link href="#tecnologia">Tecnología</Nav.Link>
-                 <Nav.Link href="#deportes">Deportes</Nav.Link>
-                 <NavDropdown title="+ Secciones" id="collasible-nav-dropdown">
-                   <NavDropdown.Item href="#action/3.1">Política</NavDropdown.Item>
-                   <NavDropdown.Item href="#action/3.2">Economía</NavDropdown.Item>
-                   <NavDropdown.Item href="#action/3.3">Salud</NavDropdown.Item>
-                   <NavDropdown.Item href="#action/3.4">Fotografías</NavDropdown.Item>
-    </NavDropdown>
-               </Nav>
-               <Nav.Link href="#acercanosotros">Acerca de Nosotros</Nav.Link>
-               <Button variant="primary" type="submit" className="w-30 m-10">
-                    Suscribirse</Button>
-               <Button variant="warning" type="submit" className="w-30">
-                    Iniciar Seción</Button>
-               
-             </Navbar.Collapse>
-             </Container>
-            </Navbar>
+          
       </>
         
     );
 };  
 
 
-export default Navigation; 
+export default Navigation;  <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
+<Container>
+<Navbar.Brand>
+  <Link to="/">
+    <img
+      src={logo}
+      width="120"
+      height="56"
+      className="d-inline-block align-top"
+      alt="<CoDiario>"
+    />
+  </Link>
+</Navbar.Brand>
+<Navbar.Toggle aria-controls="responsive-navbar-nav" />
+<Navbar.Collapse id="responsive-navbar-nav">
+  <Nav className="me-auto">
+    <Link to="/" className="nav-link">Inicio</Link>
+    <Link to="/actualidad" className="nav-link">Actualidad</Link>
+    <Link to="/especaculos" className="nav-link">Espectáculos</Link>
+    <Link to="/tecnologia" className="nav-link">Tecnología</Link>
+    <Link to="/deportes" className="nav-link">Deportes</Link>
+    <NavDropdown title="+ Secciones" id="collasible-nav-dropdown">
+      <NavDropdown.Item href="#action/3.1">Política</NavDropdown.Item>
+      <NavDropdown.Item href="#action/3.2">Economía</NavDropdown.Item>
+      <NavDropdown.Item href="#action/3.3">Salud</NavDropdown.Item>
+      <NavDropdown.Item href="#action/3.4">Fotografías</NavDropdown.Item>
+    </NavDropdown>
+  </Nav>
+  <Link to="/nosotros" className="nav-link">Acerca de Nosotros</Link>
+  <Link to="" className="btn btn-primary">Suscribirse</Link> 
+  <Link to="/login" className="btn btn-warning">Iniciar Sesión</Link>   
+             
+</Navbar.Collapse>
+</Container>
+</Navbar>
