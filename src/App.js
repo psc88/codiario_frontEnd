2ae9bfd916ca6226694ef508448addda51c2342d
AngelMarcelo <ammarce@gmail.com>
2021-12-10 22:03:40 -0300

    se corrige codigo de componente Navigation

--- src/App.js
+++ src/App.js
@@ -10,14 +10,16 @@ import 'bootstrap/dist/css/bootstrap.css';
 import 'bootstrap/dist/js/bootstrap.js';
 import $ from 'jquery';
 import Popper from 'popper.js';
+//import Login from './Pages/Login';
 
 function App() {
   return (
     <Router>
       <Navigation></Navigation>
-      <Routes>
+            <Routes>
         <Route exact path='/' element={<Inicio></Inicio>}></Route>
         <Route exact path='/nosotros' element={<AcercaNosotros></AcercaNosotros>}></Route>
+        {/*<Route exact path='/iniciarsesion' element={<Login></Login>}></Route>*/}
       </Routes>
     </Router>
   );
