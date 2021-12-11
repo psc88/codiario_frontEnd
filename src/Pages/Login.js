ca7d8b7396ad0269398b21cd378e9da31a2b4269
AngelMarcelo <ammarce@gmail.com>
2021-12-11 20:20:28 -0300

    se instala dependencia de Formik y se realiza codigo de validacion

--- src/Pages/Login.js
+++ src/Pages/Login.js
@@ -4,36 +4,120 @@ import '../App.css';
 import { Form } from "react-bootstrap";
 import {Link} from "react-router-dom";
 import {Background} from "../img/Backgroun.png";
+import { Formik } from "formik";
 
 const Login = ()=>{
- 
-    return(
-      <Container >
+
+   function Formulario() {
+      return (
+        <Container className="p-5">
+          <Card>
+            <CardHeader></CardHeader>
+            <CardBody>
+              <Formik
+                initialValues={{
+                  name: "",
+                  email: "",
+                  password: "",
+                  
+                }}
+                validate={(values) => {
+                  const errors = {};
+    
+                  // We need a name
+                  if (!values.name) errors.name = "Required";
+    
+                  // We need a valid e-mail
+                  if (!values.email) errors.email = "Required";
+                  else if (!/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i.test(values.email))
+                    errors.email = "Invalid email address";
+    
+                  // We need a valid password
+                  if (!values.password) errors.password = "Required";
+                  else if (`${values.password}`.length < 7)
+                    errors.password =
+                      "Password must be larger than 7 characters";
+    
+                  console.log({ values, errors });
+    
+                  return errors;
+                }}
+                onSubmit={(values, { setSubmitting }) => {
+                  setTimeout(() => {
+                    alert(JSON.stringify(values, null, 2));
+    
+                    setSubmitting(false);
+                  }, 250);
+                }}
+              >
+                {(props) => {
+                  const {
+                    values,
+                    errors,
+                    touched,
+                    handleChange,
+                    handleBlur,
+                    handleSubmit,
+                    isSubmitting,
+                    /* y otras más */
+                  } = props;
+
+
+     return(
+      
          {/*<img src={Background} alt="background" className='w-100'/>*/}
-      <div >
+   <div >
       
           <section id="" >
-           <Form className="my-5 center col-5">
+           <Form className="my-5 center col-5" onSubmit={handleSubmit}>
+                <Form.Group className="mb-3" controlId="formBasicName">
+                  <Form.Label for="name">Nombre y Apellido</Form.Label>
+                  <Form.Control type="text"
+                      name="name" placeholder="Ingrese su nombre y apellido" invalid={errors.name && touched.name}
+                      onChange={handleChange}
+                      onBlur={handleBlur}
+                      value={values.name}/>
+                 </Form.Group>
+
+                <Form.Group className="mb-3" controlId="formBasicDate">
+                  <Form.Label>Año de Nacimiento</Form.Label>
+                  <Form.Control type="date" placeholder="Ingrese año de nacimiento" />
+                 </Form.Group>
+
+                <Form.Group className="mb-3" controlId="formBasicName">
+                  <Form.Label>Ciudad</Form.Label>
+                  <Form.Control type="formGridCity" placeholder="Ciudad" />
+                 </Form.Group>
+
                 <Form.Group className="mb-3" controlId="formBasicEmail">
-                  <Form.Label>Correo electrónico</Form.Label>
-                  <Form.Control type="email" placeholder="Ingrese su correo electrónico " />
+                  <Form.Label for="email" name="email">Correo electrónico</Form.Label>
+                  <Form.Control type="email" placeholder="Ingrese su correo electrónico " invalid={errors.email && touched.email}
+                      onChange={handleChange}
+                      onBlur={handleBlur}
+                      value={values.email}/>
                   <Form.Text className="text-muted">
                      Nunca compartiremos su correo electrónico con nadie más.
                   </Form.Text>
                 </Form.Group>
 
                 <Form.Group className="mb-3" controlId="formBasicPassword">
-                  <Form.Label>Contraseña</Form.Label>
-                  <Form.Control type="password" placeholder="contraseña" />
+                  <Form.Label for="password" name="password">Contraseña</Form.Label>
+                  <Form.Control type="password" placeholder="contraseña" invalid={errors.password && touched.password}
+                      onChange={handleChange}
+                      onBlur={handleBlur}
+                      value={values.password}/>
                 </Form.Group>
                 
-                <Link to="/" className="btn btn-warning">Ingresar</Link>
+                <Link to="/" className="btn btn-warning" type="submit" disabled={isSubmitting} >
+                    {isSubmitting? `Loading` : `Submit`} Ingresar</Link>
             </Form>
-         </section>
-       </div>
-    </Container>
-)
-}
+            </section>
+           </div>
+     )}}
+        </Formik>
+        </CardBody>
+      </Card>
+    </Container>)}}
 
 
-export default Login;
+  export default Login;
