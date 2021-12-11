7c9191348822de9b8f758f75d6d01364ec63be7a
AngelMarcelo <ammarce@gmail.com>
2021-12-11 15:51:52 -0300

    se corrige codigo

--- src/components/common/NavbarAdmin.js
+++ src/components/common/NavbarAdmin.js
@@ -4,11 +4,8 @@ import { Link } from "react-router-dom";
 import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.css';
 import 'bootstrap/dist/js/bootstrap.js';
-import $ from 'jquery';
-import Popper from 'popper.js';
-import { NavDropdown } from "react-bootstrap";
 import logo from "../../img/Logo.png"
-import { Button } from "react-bootstrap";
+
 
 const NavbarAdmin = () =>{
     return(
--- src/components/common/NavbarUsuario.js
+++ src/components/common/NavbarUsuario.js
@@ -14,7 +14,40 @@ import { Button } from "react-bootstrap";
 const NavbarUsuario = () =>{
     return(
         <>
-          
+          <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
+             <Container>
+             <Navbar.Brand>
+               <Link to="/">
+                 <img
+                   src={logo}
+                   width="120"
+                   height="56"
+                   className="d-inline-block align-top"
+                   alt="<CoDiario>"
+                 />
+               </Link>
+             </Navbar.Brand>
+             <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+             <Navbar.Collapse id="responsive-navbar-nav">
+               <Nav className="me-auto">
+                 <Link to="/" className="nav-link">Inicio</Link>
+                 <Link to="/actualidad" className="nav-link">Actualidad</Link>
+                 <Link to="/especaculos" className="nav-link">Espectáculos</Link>
+                 <Link to="/tecnologia" className="nav-link">Tecnología</Link>
+                 <Link to="/deportes" className="nav-link">Deportes</Link>
+                 <NavDropdown title="+ Secciones" id="collasible-nav-dropdown">
+                   <NavDropdown.Item href="#action/3.1">Política</NavDropdown.Item>
+                   <NavDropdown.Item href="#action/3.2">Economía</NavDropdown.Item>
+                   <NavDropdown.Item href="#action/3.3">Salud</NavDropdown.Item>
+                   <NavDropdown.Item href="#action/3.4">Fotografías</NavDropdown.Item>
+                 </NavDropdown>
+               </Nav>
+               <Link to="/nosotros" className="nav-link">Acerca de Nosotros</Link>
+               <Link to="/contacto" className="nav-link">Contacto</Link>
+               <Link to="" className="btn btn-primary">Suscribirse</Link> 
+             </Navbar.Collapse>
+             </Container>
+            </Navbar>
       </>
         
     );
