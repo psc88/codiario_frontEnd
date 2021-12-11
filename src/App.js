3570530b4dd953387d1ec0c4a40fb2f7894d0339
AngelMarcelo <ammarce@gmail.com>
2021-12-11 12:10:56 -0300

    se limpia codigo en imports

--- src/App.js
+++ src/App.js
@@ -4,12 +4,9 @@ import Inicio from './Pages/Inicio';
 import AcercaNosotros from './Pages/AcercaNosotros';
 import {BrowserRouter as Router, Routes, Route} from 'react-router-dom'
 import 'bootstrap/dist/css/bootstrap.min.css';
-import { Navbar, Nav, Container } from "react-bootstrap";
 import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.css';
 import 'bootstrap/dist/js/bootstrap.js';
-import $ from 'jquery';
-import Popper from 'popper.js';
 import Login from './Pages/Login';
 
 function App() {
