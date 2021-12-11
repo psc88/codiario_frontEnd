21c29866ecb15bd602eb3144c2fe7dc95cf70f2a
AngelMarcelo <ammarce@gmail.com>
2021-12-11 16:01:03 -0300

    ajuste menor

--- src/App.js
+++ src/App.js
@@ -13,6 +13,7 @@ function App() {
   return (
     <Router>
       <Navigation></Navigation>
+      <Inicio></Inicio>
             <Routes>
         <Route exact path='/' element={<Inicio></Inicio>}></Route>
         <Route exact path='/nosotros' element={<AcercaNosotros></AcercaNosotros>}></Route>
