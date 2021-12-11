191a2dc34ee88bf2dc25ea1a7cc2928b14b876fc
AngelMarcelo <ammarce@gmail.com>
2021-12-11 15:34:05 -0300

    se limpia codigo en appjs

--- src/App.js
+++ src/App.js
@@ -1,21 +1,17 @@
 import './App.css';
-import Navigation from './components/common/Navigation';
+import NavbarUsuario from './components/common/NavbarUsuario';
 import Inicio from './Pages/Inicio';
 import AcercaNosotros from './Pages/AcercaNosotros';
 import {BrowserRouter as Router, Routes, Route} from 'react-router-dom'
 import 'bootstrap/dist/css/bootstrap.min.css';
-import { Navbar, Nav, Container } from "react-bootstrap";
 import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.css';
 import 'bootstrap/dist/js/bootstrap.js';
-import $ from 'jquery';
-import Popper from 'popper.js';
-import NavbarUsuario from './components/common/NavbarUsuario';
+
 
 function App() {
   return (
     <Router>
-      {/*<Navigation></Navigation>*/}
       <NavbarUsuario></NavbarUsuario>
       <Routes>
         <Route exact path='/' element={<Inicio></Inicio>}></Route>
