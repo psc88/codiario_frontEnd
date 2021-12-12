0ca4384949a2fa1b9cb48c6aa932646f456a3d60
AngelMarcelo <ammarce@gmail.com>
2021-12-11 21:24:32 -0300

    se modifica codigo en login y se agrega depenedencia de reactstrap

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.css';
 import 'bootstrap/dist/js/bootstrap.js';
-
+import "reactstrap";
 
 
 
--- src/Pages/Login.js
+++ src/Pages/Login.js
@@ -1,71 +1,68 @@
 import React from "react";
-import { Container, Form, Card} from "react-bootstrap";
+import { Container, Form} from "react-bootstrap";
 import '../App.css';
 import {Link} from "react-router-dom";
 import { Formik } from "formik";
-import CardBody from "./CardBody"
+import {Card, CardBody} from "reactstrap";
 
 
 const Login = ()=>{
+     
+        return(
 
-   function Formulario() {
-      return (
-        <Container className="p-5">
-          <Card>
-            <CardBody>
-              <Formik
-                initialValues={{
-                  name: "",
-                  email: "",
-                  password: "",
-                  
+   
+         <Container className="p-5">
+         <Card>
+           <CardBody>
+             <Formik
+               initialValues={{
+                 name: "",
+                 email: "",
+                 password: "",
                 }}
-                validate={(values) => {
-                  const errors = {};
-    
-                  // We need a name
-                  if (!values.name) errors.name = "Required";
-    
-                  // We need a valid e-mail
-                  if (!values.email) errors.email = "Required";
-                  else if (!/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i.test(values.email))
-                    errors.email = "Invalid email address";
-    
-                  // We need a valid password
-                  if (!values.password) errors.password = "Required";
-                  else if (`${values.password}`.length < 7)
-                    errors.password =
-                      "Password must be larger than 7 characters";
-    
-                  console.log({ values, errors });
-    
-                  return errors;
-                }}
-                onSubmit={(values, { setSubmitting }) => {
-                  setTimeout(() => {
-                    alert(JSON.stringify(values, null, 2));
-    
-                    setSubmitting(false);
-                  }, 250);
-                }}
-              >
-                {(props) => {
-                  const {
-                    values,
-                    errors,
-                    touched,
-                    handleChange,
-                    handleBlur,
-                    handleSubmit,
-                    isSubmitting,
-                    /* y otras más */
-                  } = props;
+               validate={(values) => {
+                 const errors = {};
+   
+                 // We need a name
+                 if (!values.name) errors.name = "Required";
+   
+                 // We need a valid e-mail
+                 if (!values.email) errors.email = "Required";
+                 else if (!/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i.test(values.email))
+                   errors.email = "Invalid email address";
+   
+                 // We need a valid password
+                 if (!values.password) errors.password = "Required";
+                 else if (`${values.password}`.length < 7)
+                   errors.password =
+                     "Password must be larger than 7 characters";
+   
+                 console.log({ values, errors });
+   
+                 return errors;
+               }}
+               onSubmit={(values, { setSubmitting }) => {
+                 setTimeout(() => {
+                   alert(JSON.stringify(values, null, 2));
+   
+                   setSubmitting(false);
+                 }, 250);
+               }}
+             >
+               {(props) => {
+                 const {
+                   values,
+                   errors,
+                   touched,
+                   handleChange,
+                   handleBlur,
+                   handleSubmit,
+                   isSubmitting,
+                   /* y otras más */
+                 } = props;
+        
 
-
-     return(
-          
-          
-           <Form className="my-5 center col-5" onSubmit={handleSubmit}>
+            <Form className="my-5 center col-5" onSubmit={handleSubmit}>
                 <Form.Group className="mb-3" controlId="formBasicName">
                   <Form.Label for="name">Nombre y Apellido</Form.Label>
                   <Form.Control type="text"
@@ -108,12 +105,12 @@ const Login = ()=>{
                     {isSubmitting? `Loading` : `Submit`} Ingresar</Link>
             </Form>
             
-           
-     )}}
+           }}
         </Formik>
         </CardBody>
       </Card>
-    </Container>)}}
+    </Container>)}
 
 
+  
   export default Login;
