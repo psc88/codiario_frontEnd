7dd1494a20649a319c02025224594d67f8961888
Epalermo <epepalermo78@gmail.com>
2021-12-13 20:27:48 -0300

    se creo pagina 404, links a revisar

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import {BrowserRouter as Router, Routes, Route} from 'react-router-dom'
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Contacto from './Pages/Contacto';
 import Footer from './Common/Footer';
+import Error404 from './Pages/Error404'
 
 
 function App() {
@@ -16,6 +17,7 @@ function App() {
         <Route exact path='/' element={<Inicio></Inicio>}></Route>
         <Route exact path='/nosotros' element={<AcercaNosotros></AcercaNosotros>}></Route>
         <Route exact path='/contacto' element={<Contacto></Contacto>}></Route>
+        <Route exact path='*' element={<Error404/>}></Route>
       </Routes>
       <Footer/>
     </Router>
