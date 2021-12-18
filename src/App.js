07cf833fc633e64ac9a3315d77615c65438ab310
AngelMarcelo <ammarce@gmail.com>
2021-12-18 00:19:35 -0300

    o

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import './App.css';
-import NavbarUsuario from './Components/common/NavbarUsuario';
+import NavbarAdmin from './Components/common/NavbarAdmin';
 import Inicio from './Pages/Inicio';
 import AcercaNosotros from './Pages/AcercaNosotros';
 import {BrowserRouter as Router, Routes, Route} from 'react-router-dom'
@@ -7,12 +7,11 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.css';
 import 'bootstrap/dist/js/bootstrap.js';
-import Login from './Pages/Login';
-import NavbarInicio from './Components/common/NavbarInicio';
 import {useState, useEffect} from 'react';
-import Contacto from './Pages/Contacto';
+import Contacto  from '../src/Pages/Contacto';
+import Error404 from '../src/Pages/Error404'
 import Footer from '../src/Components/common/Footer';
-import Error404 from './Pages/Error404';
+import NavbarUsuario from './Components/common/NavbarUsuario';
 
 
 
@@ -23,7 +22,6 @@ function App() {
   useEffect(() => {
     consultarApi();
   },[]);
-  
 
   const consultarApi = async() => {
     try{
@@ -33,23 +31,20 @@ function App() {
     } catch(error){
       console.log(error);
     }
-  };
-
+  }
 
   return (
     <Router>
-      
       <NavbarUsuario></NavbarUsuario>
       <Routes>
-      <Route exact path='/' element={<Inicio noticias={noticias} consultarApi={consultarApi}></Inicio>}></Route>
+        <Route exact path='/' element={<Inicio noticias={noticias}></Inicio>}></Route>
         <Route exact path='/nosotros' element={<AcercaNosotros></AcercaNosotros>}></Route>
         <Route exact path='/contacto' element={<Contacto></Contacto>}></Route>
-        <Route exact path='/login' element={<Login></Login>}></Route>
         <Route exact path='*' element={<Error404/>}></Route>
       </Routes>
-      <Footer/>
+        <Footer></Footer>
     </Router>
   );
-};
+}
 
 export default App;
