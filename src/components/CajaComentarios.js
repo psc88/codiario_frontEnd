808c3cdd73dea00ad29984d933bd4ff3b834b0d2
Pablo Castillo <psc88sdrd@gmail.com>
2022-01-12 22:17:53 -0300

    se quita listgroup para ver si hace deploy sin problemas

--- src/components/CajaComentarios.js
+++ src/components/CajaComentarios.js
@@ -1,16 +1,17 @@
 import React from "react";
-import { Container, ListGroup} from "react-bootstrap";
+import { Container } from "react-bootstrap";
 import Comentario from "./Comentario";
 
 
 const CajaComentarios = (props) => {
   return (
     <Container className="my-5">
-      <ListGroup>
+      <h1>hola</h1>
+      {/* <ListGroup>
         {
           props.comentarios.map((comentario)=><Comentario key={comentario.id} comentario={comentario} consultarAPI={props.consultarAPI}></Comentario>)
         }
-      </ListGroup>
+      </ListGroup> */}
     </Container>
   );
 };
