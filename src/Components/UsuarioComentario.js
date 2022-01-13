e0cc08eb843866c91a79308dad0b7d96b10931a8
Pablo Castillo <psc88sdrd@gmail.com>
2022-01-12 23:13:11 -0300

    se agrego logica de borrado del comentario en UsuarioComentario y sweetalert2

--- src/Components/UsuarioComentario.js
+++ src/Components/UsuarioComentario.js
@@ -1,13 +1,43 @@
 import React from 'react';
 import { Button, ListGroup } from "react-bootstrap";
+import Swal from 'sweetalert2';
 
 
 
 const UsuarioComentario = (props) => {
-    const eliminarProducto = () => {
-        console.log("desde elimnarProducto")
+    const eliminarComentario = () => {
+        Swal.fire({
+            title: "Estas seguro de eliminar el comentario?",
+            text: "No se puede recuperar el comentario",
+            icon: "warning",
+            showCancelButton: true,
+            confirmButtonColor: "#3085d6",
+            cancelButtonColor: "#d33",
+            confirmButtonText: "Borrar",
+            cancelButtonText: "Cancelar",
+          }).then(async (result) => {
+            if (result.isConfirmed) {
+              try {
+                const URL = process.env.REACT_APP_API_URL + "/comentarios/" + props.itemComentario.id
+                const respuesta = await fetch(URL, {
+                  method: "DELETE",
+                  headers: {
+                    "Content-Type": "apllication/json",
+                  },
+                });
+                if (respuesta.status === 200) {
+                  Swal.fire(
+                    "Comentario eliminado",
+                    "Su comentario fue eliminado correctamente",
+                    "success"
+                  );
+                  props.consultarApi();
+                }
+              } catch (error) {}
+            }
+          });
     }
-    
+
     return (
         <ListGroup.Item className="d-flex justify-content-between">
             <p>
@@ -17,7 +47,7 @@ const UsuarioComentario = (props) => {
                 {props.itemComentario.comentario}
             </p>
             <div>
-                <Button onClick={() => eliminarProducto()}>x</Button>
+                <Button onClick={() => eliminarComentario()}>X</Button>
             </div>
         </ListGroup.Item>
     );
